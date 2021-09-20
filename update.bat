f90445674b3308e20ae65860d2b67dabe218eff5
XxEray <1213097917@qq.com>
2021-09-20 09:50:42 +0800

    site: fix bat (sync)

--- update.bat
+++ update.bat
@@ -5,16 +5,16 @@ cd %~dp0
 set "msg=%1"
 if "msg" == "" set "msg=update content" 
 
-git checkout source > nul 2> nul
+git checkout source > nul
 
 echo Github: Pushing source...
-git add -A > nul 2> nul
-git commit -m %msg% > nul 2> nul
-git push origin source > nul 2> nul
+git add -A > nul
+git commit -m %msg% > nul
+git push origin source > nul
 
 echo Github: Pushing master...
-cmd /k "hexo d & exit" > nul 2> nul
+cmd /k "hexo d & exit" > nul
 
 echo Github: Cleaning...
-cmd /k "hexo clean & exit" > nul 2> nul
-rmdir /s/q ".deploy_git" > nul 2> nul
+cmd /k "hexo clean & exit" > nul
+rmdir /s/q ".deploy_git" > nul
