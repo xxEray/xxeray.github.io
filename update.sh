d30ebdb5cc0b816084133282fa7e31ed4b5a5c59
XxEray <1213097917@qq.com>
2023-09-27 09:11:47 +0800

    update content

--- update.sh
+++ update.sh
@@ -9,18 +9,16 @@ else
 	msg=$1
 fi
 
-git checkout source > nul
+git checkout source > /dev/null
 
 echo Github: Pushing source...
-git add -A > nul
-git commit -m "${msg}" > nul
-git push origin source -f > nul
+git add -A > /dev/null
+git commit -m "${msg}" > /dev/null
+git push origin source -f > /dev/null
 
 echo Github: Pushing master...
-hexo d > nul
+hexo d > /dev/null
 
 echo Github: Cleaning...
-hexo clean > nul
-rm -rf .deploy_git > nul
-
-rm nul
+hexo clean > /dev/null
+rm -rf .deploy_git > /dev/null
